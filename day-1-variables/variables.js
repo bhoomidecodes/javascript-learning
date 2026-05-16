e039c9d27a42b6c70cfa9e2e96a9a015bffc4901
bhoomidecodes <bhoomi.codes@gmail.com>
2026-05-16 17:07:30 +0530

    Create variables.js

---
+++ day-1-variables/variables.js
@@ -0,0 +1,19 @@
+let a = 10; //number
+let b = 30;
+const c = 50;
+let name = "bhoomi"; //string
+let age = 20;
+let isStudent = true;
+let salary = null;
+let marks = [10, 20, 30, 40, 50];
+let person = { name: "bhoomi", age: 20 };
+let city;
+const pi = 3.14;
+console.log(a, b, c);
+console.log(typeof name);
+console.log(typeof salary);
+console.log(typeof marks);
+console.log(typeof person);
+console.log(typeof city);
+console.log(typeof isStudent);
+console.log(typeof pi);
