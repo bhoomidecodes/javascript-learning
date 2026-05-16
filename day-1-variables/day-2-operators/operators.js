9b08e8f19d726c511ca107c9ef769d7520917868
bhoomidecodes <bhoomi.codes@gmail.com>
2026-05-16 17:08:24 +0530

    Create operators.js

---
+++ day-1-variables/day-2-operators/operators.js
@@ -0,0 +1,19 @@
+//arthmetic operations
+let addition = 2 + 4;
+let subtract = 9 - 3;
+let multi = 4 * 8;
+let div = 6 / 2;
+let remainder = 9 % 2;
+let exponintial = 2 ** 3;
+let myscore = 110;
+myscore++;
+++myscore;
+let credits = 500;
+credits--;
+
+//comparison operations
+let num_1 = 3;
+let num_2 = 6;
+let num_3 = 5;
+console.log(num_1 == num_2);
+console.log(num_1 != num_3);
