22baccc00251d01e8438858ba75003eca6415562
bhoomi sharma <bhoomi.codes@gmail.com>
2026-05-25 14:25:08 +0530

    Create objects_01.js

---
+++ day-8/objects_01.js
@@ -0,0 +1,11 @@
+const username = {
+  firstname: "bhoomi",
+  age: 20,
+};
+console.log(username.firstname);
+console.log(username);
+username.firstname = "mohini";
+username.lastname = "sharma";
+console.log(username);
+console.log(username.lastname);
+//console.log(typeof obj);
