1ae3a6e2317d43f3ba07f38a32a21858c61729ed
bhoomi sharma <bhoomi.codes@gmail.com>
2026-05-26 16:39:21 +0530

    Create string.js

---
+++ day-9/string.js
@@ -0,0 +1,6 @@
+const name = "bhoomi";
+const repocount = 40;
+console.log(name + repocount + "value");
+console.log(`hell0 my name is ${name} and my repo count is $ {repocount}`);
+const gameName = new String("bhoomi");
+console.log(gameName[0]);
